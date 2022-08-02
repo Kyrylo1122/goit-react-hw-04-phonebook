01dde614c3f1ac63f57fbaab3c5351facd56cec0
Kyrylo1122 <93613807+Kyrylo1122@users.noreply.github.com>
2022-08-02 19:54:34 +0200

    first try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,46 @@
-import { Component } from 'react';
-import { ContactForm } from './ContactForm/Contactform';
+import { useState, useEffect } from 'react';
+import ContactForm from './ContactForm/Contactform';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { Box } from './Box/Box';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-  componentDidMount() {
-    const items = localStorage.getItem('contacts');
-    const parsedItems = JSON.parse(items);
-    if (parsedItems) {
-      this.setState({ contacts: [...parsedItems] });
-    }
-  }
-  componentDidUpdate(_, prevState) {
-    const newContacts = this.state.contacts;
-    if (newContacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(newContacts));
-    }
-  }
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-  onSubmitForm = data => {
-    const isExist = this.isExistedName(data.name);
+export default function App() {
+  const [contacts, setContacts] = useState(() => {
+    const parsedItems = JSON.parse(localStorage.getItem('contacts'));
+    return parsedItems ? parsedItems : [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const changeFilter = e => setFilter(e.currentTarget.value);
+
+  const onSubmitForm = data => {
+    const isExist = contacts.map(e => e.name).includes(data.name);
     if (isExist) {
       alert('Name already exist');
       return;
     }
-
-    this.setState(prevState => ({ contacts: [...prevState.contacts, data] }));
+    setContacts(prevState => [...prevState, data]);
   };
-  isExistedName = name => this.state.contacts.map(e => e.name).includes(name);
 
-  deleteContact = id => {
-    this.setState(p => ({ contacts: p.contacts.filter(e => e.id !== id) }));
-  };
-  render() {
-    const { filter, contacts } = this.state;
-    const visibleContacts = contacts.filter(e =>
-      e.name.toLowerCase().includes(filter.toLowerCase())
-    );
-    return (
-      <Box display="flex">
-        <Box ml="auto" mr="auto" textAlign="center">
-          <h1>Phonebook</h1>
-          <ContactForm onSubmitForm={this.onSubmitForm} />
-          <h2>Contacts</h2>
-          <Filter changeFilter={this.changeFilter} />
-          <ContactList
-            contacts={visibleContacts}
-            deleteCon={this.deleteContact}
-          />
-        </Box>
+  const deleteContact = id =>
+    setContacts(prevState => prevState.filter(e => e.id !== id));
+
+  const visibleContacts = contacts.filter(e =>
+    e.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  return (
+    <Box display="flex">
+      <Box ml="auto" mr="auto" textAlign="center">
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitForm={onSubmitForm} />
+        <h2>Contacts</h2>
+        <Filter changeFilter={changeFilter} />
+        <ContactList contacts={visibleContacts} deleteCon={deleteContact} />
       </Box>
-    );
-  }
+    </Box>
+  );
 }
-export default App;
--- src/components/ContactForm/Contactform.jsx
+++ src/components/ContactForm/Contactform.jsx
@@ -1,73 +1,48 @@
-import { Component } from 'react';
 import { nanoid } from 'nanoid';
-
+import { validateName, validateTel } from './ValidateForm';
 import { Formik, Field, ErrorMessage } from 'formik';
 import { FormikForm, ErrorText, ButtonSubmit } from './Contactform.styled';
 import { scheme } from 'components/const/Scheme';
-import { initialValue } from 'components/const/InitialValue';
+const initialValue = {
+  name: '',
+  number: '',
+  id: '',
+};
+export default function ContactForm({ onSubmitForm }) {
+  const nameId = nanoid();
+  const telId = nanoid();
 
-export class ContactForm extends Component {
-  state = { ...initialValue };
-  handleSetContacts = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = (value, { resetForm }) => {
+  const handleSubmit = (value, { resetForm }) => {
     const id = nanoid();
     value.id = id;
-    this.props.onSubmitForm(value);
+    onSubmitForm(value);
+    console.log(value);
     resetForm();
   };
-  validateName = value =>
-    !/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/i.test(value)
-      ? "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-      : '';
-  validateTel = value =>
-    !/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/i.test(
-      value
-    )
-      ? 'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-      : '';
-  render() {
-    const nameId = nanoid();
-    const telId = nanoid();
 
-    const FormError = ({ name }) => {
-      return (
-        <ErrorMessage
-          name={name}
-          render={message => <ErrorText>{message}</ErrorText>}
-        />
-      );
-    };
-    return (
-      <Formik
-        onSubmit={this.handleSubmit}
-        initialValues={initialValue}
-        validationSchema={scheme}
-      >
-        <FormikForm>
-          <label htmlFor={nameId}>Name</label>
-          <Field
-            type="text"
-            validate={this.validateName}
-            name="name"
-            id={nameId}
-          />
-          <FormError name="name" />
-          <label htmlFor={telId}>Phone number</label>
-          <Field
-            type="tel"
-            validate={this.validateTel}
-            name="number"
-            id={telId}
-          />
-          <FormError name="number" />
+  const FormError = ({ name }) => (
+    <ErrorMessage
+      name={name}
+      render={message => <ErrorText>{message}</ErrorText>}
+    />
+  );
 
-          <ButtonSubmit type="submit">Add contact</ButtonSubmit>
-        </FormikForm>
-      </Formik>
-    );
-  }
+  return (
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={initialValue}
+      validationSchema={scheme}
+    >
+      <FormikForm>
+        <label htmlFor={nameId}>Name</label>
+        <Field type="text" validate={validateName} name="name" id={nameId} />
+        <FormError name="name" />
+        <label htmlFor={telId}>Phone number</label>
+        <Field type="tel" validate={validateTel} name="number" id={telId} />
+        <FormError name="number" />
+
+        <ButtonSubmit type="submit">Add contact</ButtonSubmit>
+      </FormikForm>
+    </Formik>
+  );
 }
